9dae6fb407b58e141bf26f2bf75b20b6cd15fd11
Moshe Stern <moshecstern@gmail.com>
2019-11-25 18:53:33 -0500

    adding jawsdb forheroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,18 @@
 require("dotEnv").config();
 var mysql = require("mysql");
 
-
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.SQL_PASS,
   database: "tracker_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
