935170858c57e2d352e4a1f7163d7bf0e90da5e4
Moshe Stern <moshecstern@gmail.com>
2019-11-26 13:47:04 -0500

    css for heroku 5

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname+"/public"));
+app.use(express.static("public"));
 
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
