ac8deaccc370f71c8b8f71314c048cd4665f5283
Moshe Stern <moshecstern@gmail.com>
2019-11-26 13:34:28 -0500

    heroku fixagain

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/coffee_Controller.js");
+var routes = require("./controllers/coffee_controller.js");
 
 app.use(routes);
 
